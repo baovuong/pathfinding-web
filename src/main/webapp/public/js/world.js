95a299429ff15dc97bed549bd0cad40deb22d111
Bao Vuong <vqbao@lv5.org>
2017-01-03 00:40:21 -0500

    got world to render

--- src/main/webapp/public/js/world.js
+++ src/main/webapp/public/js/world.js
@@ -1,43 +1,48 @@
-
-var World = function(width, height) {
-    this.width = width;
-    this.height = height;
-    this.grid = new Array(height);
-    for (var i=0; i<height; i++) {
-    	this.grid[0] = new Array(width);
-    }
-}
-
-World.prototype.getStart() {
-	
-}
-
-World.prototype.getGoal() {
-	
-}
-
-World.prototype.getObstacles() {
-	
-}
-
-World.prototype.createElement = function() {
-    var world = document.createElement("table");
-    world.className = "world";
-
+function initWorld(width, height) {
+    var world = document.getElementById('world');
     // add cells
-    for (var r = 0; r < this.height; ++r) {
-        var row = document.createElement("tr");
-        for (var c = 0; c < this.width; ++c) {
-            var column = document.createElement("td");
-            //column.style.width = '1px'; //''+100/this.width+'%';
+    for (var r = 0; r < height; ++r) {
+        var row = document.createElement('tr');
+        for (var c = 0; c < width; ++c) {
+            var column = document.createElement('td');
+            // column.style.width = '1px'; //''+100/this.width+'%';
             column.innerHTML = '&nbsp;';
-            column.addEventListener('click', function() {
-            	// toggle between obstacle
-            });
+            
+            if ((r == 0 && c == 0) || (r == height-1 && c == width-1)) {
+                column.className = 'point';
+            } else {
+                column.onclick = function(e) {
+                    var sender = e.srcElement || e.target;
+                    if (sender.className == 'obstacle') {
+                        sender.className = '';
+                    } else {
+                        sender.className = 'obstacle';
+                    }
+                }             
+            }
+
             row.appendChild(column);
         }
         world.appendChild(row);
     }
-    
-    return world;    
-};
+}
+
+function getAlgorithms(select) {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'api/algorithms', true);
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var result = JSON.parse(this.responseText);
+            for (var i=0; i<result.length; i++) {
+                var option = document.createElement('option');
+                option.text = result[i];
+                select.add(option);
+            }
+        }
+    }
+    request.send();
+}
+
+function findPath(algorithm) {
+    var world = document.getElementById('world');
+}
