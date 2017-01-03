1182463ee16adf035ef286d2b1e338d4b3c15e80
Bao Vuong <vqbao@lv5.org>
2017-01-03 02:16:07 -0500

    finished initial test client, but found a bug.

--- src/main/webapp/public/js/world.js
+++ src/main/webapp/public/js/world.js
@@ -7,8 +7,8 @@ function initWorld(width, height) {
             var column = document.createElement('td');
             // column.style.width = '1px'; //''+100/this.width+'%';
             column.innerHTML = '&nbsp;';
-            
-            if ((r == 0 && c == 0) || (r == height-1 && c == width-1)) {
+
+            if ((r == 0 && c == 0) || (r == height - 1 && c == width - 1)) {
                 column.className = 'point';
             } else {
                 column.onclick = function(e) {
@@ -18,7 +18,7 @@ function initWorld(width, height) {
                     } else {
                         sender.className = 'obstacle';
                     }
-                }             
+                }
             }
 
             row.appendChild(column);
@@ -33,7 +33,7 @@ function getAlgorithms(select) {
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var result = JSON.parse(this.responseText);
-            for (var i=0; i<result.length; i++) {
+            for (var i = 0; i < result.length; i++) {
                 var option = document.createElement('option');
                 option.text = result[i];
                 select.add(option);
@@ -45,4 +45,46 @@ function getAlgorithms(select) {
 
 function findPath(algorithm) {
     var world = document.getElementById('world');
+    var rows = world.getElementsByTagName('tr');
+    var height = rows.length;
+    var width = rows[0].getElementsByTagName('td').length;
+
+    var obstacles = new Array();
+    var cells = world.getElementsByTagName('td');
+    for (var i = 1; i <= cells.length; i++) {
+        if (cells[i - 1].className == 'obstacle') {
+            // add it to obstacles
+            var y = i / width + 1;
+            var x = i % width;
+            x = x == 0 ? width : x;
+            obstacles.push({
+                'y' : y,
+                'x' : x
+            });
+        }
+    }
+    
+    var request = new XMLHttpRequest();
+    request.open('POST', 'api/solve/'+algorithm, true);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(this.responseText);
+            var result = JSON.parse(this.responseText);
+            var path = result.path;
+            for (var i=0; i<path.length; i++) {
+                var cellIndex = (path[i].y-1)*width + path[i].x - 1;
+                if (cells[cellIndex].className == '')
+                    cells[cellIndex].className = 'path';
+            }
+        }
+    }  
+    request.send(JSON.stringify({
+        width: width,
+        height: height,
+        start: {x: 1, y: 1},
+        goal: {x: width, y: height},
+        obstacles: obstacles
+    }));       
+
 }
